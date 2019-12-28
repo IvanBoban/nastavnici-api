753f8661d1d2d6a6fd720aa64f75dfe923fed610
Ivan Boban <ivan.boban911@gmail.com>
2019-12-28 13:43:23 +0100

    added cors

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express=require ('express');
 const app = express();
 const array = require('./nastavnci_list')
 const port = process.env.PORT || 4000 ;
-
+const cors = require('cors');
+app.use(cors())
 app.get('/', (req,res)=>{
     res.send(array)
 })
