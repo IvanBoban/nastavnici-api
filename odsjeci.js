b344ae99f039fffbf70a9f8bbc2db0b10f740dbd
Ivan Boban <ivan.boban911@gmail.com>
2020-01-07 17:56:15 +0100

    added meta list for all departments

---
+++ odsjeci.js
@@ -0,0 +1,23 @@
+module.exports=[
+    
+    {
+        id: 1,
+        name: 'Anglistika'
+    },
+    {
+        id: 2,
+        name: 'Arhivistika'
+    },
+    {
+        id: 3,
+        name: 'Filozofija'
+    },
+    {
+        id: 4,
+        name: 'Informacijske i komunikacijske znanosti'
+    },
+    {
+        id: 5,
+        name: 'Skandinavistika'
+    }
+]
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express=require ('express');
 const app = express();
+const odsjeci = require('./odsjeci')
 const nastavnci_inf = require('./nastavnci_inf');
 const nastavnci_ang = require('./nastavnici_ang');
 const nastavnci_skan = require('./nastavnici_skan');
@@ -7,23 +8,27 @@ const nastavnci_arh= require('./nastavnici_arh');
 const nastavnci_filo = require('./nastavnici_filo')
 const port = process.env.PORT || 4000 ;
 const cors = require('cors');
-app.use(cors())
+app.use(cors());
+
+app.get('/odsjeci', (req,res)=>{
+    res.send(odsjeci)
+});
 app.get('/', (req,res)=>{
     res.send(nastavnci_inf)
-})
+});
 app.get('/inf', (req,res)=>{
     res.send(nastavnci_inf)
-})
+});
 app.get('/ang', (req,res)=>{
     res.send(nastavnci_ang)
-})
+});
 app.get('/skan', (req,res)=>{
     res.send(nastavnci_skan)
-})
+});
 app.get('/arh', (req,res)=>{
     res.send(nastavnci_arh)
-})
+});
 app.get('/filo', (req,res)=>{
     res.send(nastavnci_filo)
-})
+});
 app.listen(port, ()=> console.log(`listening on ${port}`))
